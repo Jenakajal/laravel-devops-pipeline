07bc477aafc18346bc72d873a56de19e2ccf8b8f
EC2 Default User <ec2-user@ip-172-31-10-108.ap-south-1.compute.internal>
2025-05-08 20:19:18 +0000

    Added both jenkins and Terraform file

---
+++ variables.tf
@@ -0,0 +1,67 @@
+variable "region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+  default     = "eks-cluster"
+}
+
+variable "cluster_version" {
+  description = "EKS cluster version"
+  type        = string
+  default     = "1.29"
+}
+
+variable "node_group_instance_types" {
+  description = "List of instance types for the EKS node group"
+  type        = list(string)
+  default     = ["t3.medium"]
+}
+
+variable "desired_size" {
+  description = "Desired number of worker nodes in the node group"
+  type        = number
+  default     = 2
+}
+
+variable "min_size" {
+  description = "Minimum number of worker nodes in the node group"
+  type        = number
+  default     = 1
+}
+
+variable "max_size" {
+  description = "Maximum number of worker nodes in the node group"
+  type        = number
+  default     = 3
+}
+
+# Variable to define the name of the node group, for consistency
+variable "node_group_name" {
+  description = "Name of the EKS node group"
+  type        = string
+  default     = "jenkins_nodes"  # Updated to match the Jenkins node group name
+}
+
