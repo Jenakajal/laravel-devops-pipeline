d4ceff27391b9866d42fb430f7d22facc4b9b2d8
EC2 Default User <ec2-user@ip-172-31-10-108.ap-south-1.compute.internal>
2025-05-08 19:25:49 +0000

    Added file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,61 +1,55 @@
 terraform {
-  required_version = ">= 1.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-west-2"  # change to your region
+  region = var.region
 }
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.2"
 
-  name = "eks-vpc"
-  cidr = "10.0.0.0/16"
+  name = "${var.cluster_name}-vpc"
+  cidr = var.vpc_cidr
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
 
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
-    Name        = "eks-vpc"
-    Environment = "dev"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
   }
 }
 
+data "aws_availability_zones" "available" {}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.11.0"
+  version = "20.13.0"
 
-  cluster_name    = "eks-cluster"
-  cluster_version = "1.29"
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
 
-  cluster_endpoint_public_access = true
-
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.private_subnets
-
-  enable_irsa = true
+  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
 
   eks_managed_node_groups = {
-    default = {
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-
-      instance_types = ["t3.medium"]
-
-      capacity_type = "ON_DEMAND"
+    default_node_group = {
+      instance_types = var.node_group_instance_types
+      desired_size   = var.desired_size
+      min_size       = var.min_size
+      max_size       = var.max_size
     }
   }
 
@@ -70,7 +64,8 @@ output "cluster_name" {
 }
 
 output "kubeconfig" {
-  value = module.eks.kubeconfig_filename
+  value     = module.eks.kubeconfig
+  sensitive = true
 }
 
 output "vpc_id" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,78 +1,50 @@
-terraform {
-  required_version = ">= 1.3"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-  }
+variable "region" {
+  description = "AWS Region"
+  default     = "us-west-2"
 }
 
-provider "aws" {
-  region = "us-west-2"  # change to your region
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
 }
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.2"
-
-  name = "eks-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = {
-    Name        = "eks-vpc"
-    Environment = "dev"
-  }
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "20.11.0"
-
-  cluster_name    = "eks-cluster"
-  cluster_version = "1.29"
-
-  cluster_endpoint_public_access = true
-
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.private_subnets
-
-  enable_irsa = true
-
-  eks_managed_node_groups = {
-    default = {
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-
-      instance_types = ["t3.medium"]
-
-      capacity_type = "ON_DEMAND"
-    }
-  }
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 
-output "cluster_name" {
-  value = module.eks.cluster_name
+variable "cluster_name" {
+  description = "EKS cluster name"
+  default     = "eks-cluster"
 }
 
-output "kubeconfig" {
-  value = module.eks.kubeconfig_filename
+variable "cluster_version" {
+  description = "EKS cluster version"
+  default     = "1.29"
 }
 
-output "vpc_id" {
-  value = module.vpc.vpc_id
+variable "node_group_instance_types" {
+  description = "List of instance types for node group"
+  default     = ["t3.medium"]
 }
+
+variable "desired_size" {
+  description = "Desired node group size"
+  default     = 2
+}
+
+variable "min_size" {
+  description = "Minimum node group size"
+  default     = 1
+}
+
+variable "max_size" {
+  description = "Maximum node group size"
+  default     = 3
+}
+
