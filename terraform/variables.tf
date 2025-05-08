c8e127f35a7c8605c6867c270dcc318003e53ad9
EC2 Default User <ec2-user@ip-172-31-10-108.ap-south-1.compute.internal>
2025-05-08 18:30:46 +0000

    Added file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,22 +1,24 @@
-# variable.tf
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-west-2"
-}
-
+# Variable for VPC ID
 variable "vpc_id" {
   description = "VPC ID for the EKS Cluster"
   type        = string
+  default     = "vpc-xxxxxxxx"  # Replace with your actual VPC ID
 }
 
+# Variable for Subnets
 variable "subnets" {
   description = "List of subnet IDs for the EKS cluster"
   type        = list(string)
+  default     = [
+    "subnet-xxxxxxxx",  # Replace with your actual subnet IDs
+    "subnet-yyyyyyyy"
+  ]
 }
 
+# Variable for Cluster Name
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
+  default     = "my-eks-cluster"  # Replace with your desired EKS cluster name
 }
 
