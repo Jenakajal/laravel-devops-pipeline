6b6d249b6660b3ad5f26bc7cb28ee005992919f5
EC2 Default User <ec2-user@ip-172-31-10-108.ap-south-1.compute.internal>
2025-05-09 09:41:35 +0000

    Added files

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,8 @@ provider "aws" {
   region = var.region
 }
 
+data "aws_availability_zones" "available" {}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.2"
@@ -48,52 +50,5 @@ module "eks" {
   }
 }
 
-resource "aws_iam_role" "this" {
-  name               = "${var.cluster_name}-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
-
-  tags = {
-    Name = "${var.cluster_name}-role"
-  }
-}
-
-resource "aws_iam_role_policy" "this" {
-  role   = aws_iam_role.this.id
-  policy = data.aws_iam_policy_document.role_policy.json
-}
-
-resource "aws_kms_alias" "this" {
-  name          = "alias/eks/${var.cluster_name}"
-  target_key_id = aws_kms_key.this.key_id
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_cloudwatch_log_group" "this" {
-  name = "/aws/eks/${var.cluster_name}/cluster"
-
-  lifecycle {
-    prevent_destroy     = true
-    create_before_destroy = true
-  }
-}
-
-output "cluster_name" {
-  value = module.eks.cluster_name
-}
-
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
-output "cluster_certificate_authority_data" {
-  value     = module.eks.cluster_certificate_authority_data
-  sensitive = true
-}
-
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
+resource "aws_iam_
 
