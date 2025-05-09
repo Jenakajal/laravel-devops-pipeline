e126effb8aa5d774e79318efca091361b5e36e11
EC2 Default User <ec2-user@ip-172-31-10-108.ap-south-1.compute.internal>
2025-05-09 07:40:28 +0000

    added fix files

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,11 +4,10 @@ provider "aws" {
 
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
-  enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "eks-vpc"
+    Name = "main-vpc"
   }
 }
 
@@ -16,7 +15,7 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "eks-igw"
+    Name = "main-igw"
   }
 }
 
@@ -25,14 +24,12 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidrs[count.index]
   map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  availability_zone       = element(var.azs, count.index)
 
   tags = {
-    Name = "eks-public-subnet-${count.index}"
+    Name = "public-subnet-${count.index}"
   }
 }
-
-data "aws_availability_zones" "available" {}
 
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eks-cluster-role"
@@ -40,11 +37,11 @@ resource "aws_iam_role" "eks_cluster_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
+      Effect = "Allow",
       Principal = {
         Service = "eks.amazonaws.com"
-      }
+      },
+      Action = "sts:AssumeRole"
     }]
   })
 }
@@ -71,26 +68,26 @@ resource "aws_iam_role" "eks_node_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
+      Effect = "Allow",
       Principal = {
         Service = "ec2.amazonaws.com"
-      }
+      },
+      Action = "sts:AssumeRole"
     }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodePolicy" {
+resource "aws_iam_role_policy_attachment" "node_worker" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOnly" {
+resource "aws_iam_role_policy_attachment" "node_ecr" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
+resource "aws_iam_role_policy_attachment" "node_cni" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
@@ -103,34 +100,34 @@ resource "aws_eks_node_group" "eks_managed_node_group" {
 
   scaling_config {
     desired_size = 2
-    max_size     = 2
+    max_size     = 3
     min_size     = 1
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryReadOnly,
-    aws_iam_role_policy_attachment.node_AmazonEKS_CNI_Policy
+    aws_iam_role_policy_attachment.node_worker,
+    aws_iam_role_policy_attachment.node_ecr,
+    aws_iam_role_policy_attachment.node_cni
   ]
-}
-
-output "eks_cluster_name" {
-  value = aws_eks_cluster.eks.name
 }
 
 output "eks_cluster_endpoint" {
   value = aws_eks_cluster.eks.endpoint
 }
 
+output "eks_cluster_name" {
+  value = aws_eks_cluster.eks.name
+}
+
 output "eks_node_group_status" {
   value = aws_eks_node_group.eks_managed_node_group.status
 }
 
-output "subnet_ids" {
-  value = aws_subnet.public[*].id
-}
-
 output "vpc_id" {
   value = aws_vpc.main.id
+}
+
+output "subnet_ids" {
+  value = aws_subnet.public[*].id
 }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,21 +1,27 @@
 variable "aws_region" {
-  description = "AWS region to deploy resources"
+  description = "AWS region"
   default     = "us-west-2"
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+  description = "VPC CIDR block"
   default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
-  description = "List of CIDR blocks for the public subnets"
+  description = "List of public subnet CIDRs"
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
+variable "azs" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["us-west-2a", "us-west-2b"]
+}
+
 variable "cluster_name" {
-  description = "Name of the EKS cluster"
+  description = "EKS cluster name"
   default     = "eks-cluster"
 }
 
