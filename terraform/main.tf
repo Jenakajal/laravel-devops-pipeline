4684897363ac651e18ef87fb33316ef9587f784f
EC2 Default User <ec2-user@ip-172-31-10-108.ap-south-1.compute.internal>
2025-05-09 07:47:25 +0000

    added fix files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,54 +1,5 @@
 provider "aws" {
-  region = var.aws_region
-}
-
-resource "aws_vpc" "main" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "main-vpc"
-  }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main-igw"
-  }
-}
-
-resource "aws_subnet" "public" {
-  count                   = 2
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.public_subnet_cidrs[count.index]
-  map_public_ip_on_launch = true
-  availability_zone       = element(var.azs, count.index)
-
-  tags = {
-    Name = "public-subnet-${count.index}"
-  }
-}
-
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "eks.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  region = "us-west-2"  # Update this to your preferred region
 }
 
 resource "aws_eks_cluster" "eks" {
@@ -57,77 +8,103 @@ resource "aws_eks_cluster" "eks" {
 
   vpc_config {
     subnet_ids = aws_subnet.public[*].id
+    security_group_ids = [aws_security_group.eks_security_group.id]
   }
 
-  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy_attach]
+}
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attach" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+
+  depends_on = [aws_iam_role.eks_cluster_role]
 }
 
 resource "aws_iam_role" "eks_node_role" {
   name = "eks-node-role"
-
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "ec2.amazonaws.com"
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
       },
-      Action = "sts:AssumeRole"
-    }]
+    ]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "node_worker" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  
+  depends_on = [aws_iam_role.eks_node_role]
 }
 
 resource "aws_iam_role_policy_attachment" "node_ecr" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+
+  depends_on = [aws_iam_role.eks_node_role]
 }
 
 resource "aws_iam_role_policy_attachment" "node_cni" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+
+  depends_on = [aws_iam_role.eks_node_role]
 }
 
-resource "aws_eks_node_group" "eks_managed_node_group" {
-  cluster_name    = aws_eks_cluster.eks.name
-  node_group_name = "eks-node-group"
-  node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = aws_subnet.public[*].id
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+}
 
-  scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
+resource "aws_subnet" "public" {
+  count = 2
+  vpc_id     = aws_vpc.main.id
+  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "eks_security_group" {
+  name        = "eks-security-group"
+  description = "Allow inbound traffic for EKS"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  depends_on = [
-    aws_iam_role_policy_attachment.node_worker,
-    aws_iam_role_policy_attachment.node_ecr,
-    aws_iam_role_policy_attachment.node_cni
-  ]
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-output "eks_cluster_endpoint" {
-  value = aws_eks_cluster.eks.endpoint
-}
-
-output "eks_cluster_name" {
-  value = aws_eks_cluster.eks.name
-}
-
-output "eks_node_group_status" {
-  value = aws_eks_node_group.eks_managed_node_group.status
-}
-
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "subnet_ids" {
-  value = aws_subnet.public[*].id
-}
+data "aws_availability_zones" "available" {}
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,27 +1,12 @@
-variable "aws_region" {
-  description = "AWS region"
-  default     = "us-west-2"
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
+  type        = string
+  default     = "eks-cluster"
 }
 
 variable "vpc_cidr" {
-  description = "VPC CIDR block"
+  description = "CIDR block for the VPC"
+  type        = string
   default     = "10.0.0.0/16"
-}
-
-variable "public_subnet_cidrs" {
-  description = "List of public subnet CIDRs"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
-variable "azs" {
-  description = "Availability zones"
-  type        = list(string)
-  default     = ["us-west-2a", "us-west-2b"]
-}
-
-variable "cluster_name" {
-  description = "EKS cluster name"
-  default     = "eks-cluster"
 }
 
